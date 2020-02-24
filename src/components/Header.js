3e0045c8fe07d433b702778d431228f04da185bc
Gareth Peat <gp16@sanger.ac.uk>
2020-02-24 11:07:32 +0000

    Update Header links to internal pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box, Grid, Button, Link } from '@material-ui/core';
+import { Box, Grid, Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <Box m={1} color="white">
@@ -8,36 +9,16 @@ const Header = () => (
         <img src="JunoLogo.svg" height="57px" />
       </Grid>
       <Grid item>
-        <Button
-          color="inherit"
-          target="_blank"
-          href="https://www.pneumogen.net/gps/"
-          component={Link}
-        >
+        <Button color="inherit" to="/" component={Link}>
           Home
         </Button>
-        <Button
-          color="inherit"
-          target="_blank"
-          href="https://www.pneumogen.net/gps/"
-          component={Link}
-        >
+        <Button color="inherit" to="/team" component={Link}>
           Team
         </Button>
-        <Button
-          color="inherit"
-          target="_blank"
-          href="https://www.pneumogen.net/gps/"
-          component={Link}
-        >
+        <Button color="inherit" to="/locations" component={Link}>
           Where we work
         </Button>
-        <Button
-          color="inherit"
-          target="_blank"
-          href="https://www.pneumogen.net/gps/"
-          component={Link}
-        >
+        <Button color="inherit" href="/partners" component={Link}>
           Partners
         </Button>
       </Grid>
