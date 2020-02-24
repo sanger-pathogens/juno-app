41a1fc6a0dfe4b3b7fb8af89352b20ff73e10460
Gareth Peat <gp16@sanger.ac.uk>
2020-02-24 10:02:52 +0000

    Add draft Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Box, Grid, IconButton, Link } from '@material-ui/core';
+import AlternateEmailOutlinedIcon from '@material-ui/icons/AlternateEmailOutlined';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
+const Footer = () => (
+  <Box m={1} color="white">
+    <Grid container alignItems="center" justify="space-between" spacing={24}>
+      <Grid item>
+        <img src="SangerLogo.9423243b.png" width="166" />
+      </Grid>
+      <Grid item>
+        <IconButton
+          color="inherit"
+          target="_blank"
+          href="https://www.pneumogen.net/gps/"
+          component={Link}
+        >
+          <AlternateEmailOutlinedIcon />
+        </IconButton>
+        <IconButton
+          color="inherit"
+          target="_blank"
+          href="https://www.pneumogen.net/gps/"
+          component={Link}
+        >
+          <LinkedInIcon />
+        </IconButton>
+        <IconButton
+          color="inherit"
+          target="_blank"
+          href="https://www.pneumogen.net/gps/"
+          component={Link}
+        >
+          <TwitterIcon />
+        </IconButton>
+      </Grid>
+    </Grid>
+  </Box>
+);
+
+export default Footer;
