f2a840360faa70bbcfa60b8086892b62a91403fb
Gareth Peat <gp16@sanger.ac.uk>
2020-03-24 13:23:53 +0000

    Use CssBaseline

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { CssBaseline } from '@material-ui/core';
 
 import PageHome from './components/PageHome';
 import PageLocations from './components/PageLocations';
@@ -8,22 +9,25 @@ import PageTeam from './components/PageTeam';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <PageHome />
-        </Route>
-        <Route path="/partners">
-          <PagePartners />
-        </Route>
-        <Route path="/team">
-          <PageTeam />
-        </Route>
-        <Route path="/locations">
-          <PageLocations />
-        </Route>
-      </Switch>
-    </Router>
+    <React.Fragment>
+      <CssBaseline />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <PageHome />
+          </Route>
+          <Route path="/partners">
+            <PagePartners />
+          </Route>
+          <Route path="/team">
+            <PageTeam />
+          </Route>
+          <Route path="/locations">
+            <PageLocations />
+          </Route>
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
