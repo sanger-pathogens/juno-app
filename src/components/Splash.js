14b4dfcde4f86a863572d6b902097b6ec5fdcfa3
Gareth Peat <gp16@sanger.ac.uk>
2020-04-08 17:40:38 +0100

    Add icons to Splash

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Grid, Box, Typography } from '@material-ui/core';
+import { Grid, Box, Typography, Hidden } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { ArrowDownward } from '@material-ui/icons';
 import ReactMarkdown from 'react-markdown';
 import raw from 'raw.macro';
 
 import Triangles from './Triangles';
+import IconWorld from './IconWorld';
+import IconStreptococcus from './IconStreptococcus';
+import IconPhylogeny from './IconPhylogeny';
 
 const useStyles = makeStyles(theme => ({
   splashTop: {
@@ -20,8 +23,8 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const renderers = {
-  root: ({ children }) => (
-    <Typography variant={`h6`} align="justify">
+  paragraph: ({ children }) => (
+    <Typography align="justify" gutterBottom>
       {children}
     </Typography>
   ),
@@ -46,7 +49,7 @@ const Splash = () => {
           <Box align="center" pt={4}>
             <img src="JunoLogo.svg" height="160px" alt="JUNO" />
           </Box>
-          <Box align="center">
+          <Box align="center" pl={1} pr={1}>
             <Typography variant="h5" component="h2">
               A global genomic survey of <i>Streptococcus agalactiae</i>
             </Typography>
@@ -66,10 +69,27 @@ const Splash = () => {
           xs={12}
           alignItems="center"
           justify="center"
-          style={{ height: '60vh' }}
+          style={{ height: '60vh', zIndex: 30 }}
         >
+          <Hidden smDown>
+            <Grid item xs={12}>
+              <Box align="center">
+                <Box p={1} display="inline">
+                  <IconWorld />
+                </Box>
+                <Box p={1} display="inline">
+                  <IconStreptococcus />
+                </Box>
+                <Box p={1} display="inline">
+                  <IconPhylogeny />
+                </Box>
+              </Box>
+            </Grid>
+          </Hidden>
           <Grid item xs={12} sm={10} md={8} lg={6}>
-            <MarkdownIntro md={raw('../content/intro.md')} />
+            <Box pl={1} pr={1}>
+              <MarkdownIntro md={raw('../content/intro.md')} />
+            </Box>
           </Grid>
           <Grid item xs={12}>
             <Typography align="center">
