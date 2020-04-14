a9133ec3249964e1f472a8d53fe6ebeeccf58d2c
Gareth Peat <gp16@sanger.ac.uk>
2020-04-14 13:48:23 +0100

    Fix min/max css issue

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,41 +1,31 @@
 import React from 'react';
-import { Grid, Box, Typography } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-import Triangles from './Triangles';
-
-const topHeight = 'max(40vh, 330px)';
-
 const useStyles = makeStyles(theme => ({
-  splashTop: {
+  splash: {
+    backgroundImage: 'url(triangles.svg)',
+    backgroundPosition: 'center top',
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed',
+    backgroundSize: 'cover',
     backgroundColor: theme.palette.primary.main,
-    height: topHeight,
+    height: '330px',
   },
 }));
 
 const Splash = () => {
   const classes = useStyles();
   return (
-    <Box width="100%" height={topHeight}>
-      <Triangles containerHeight={() => topHeight} />
-      <Grid
-        container
-        justify="center"
-        alignItems="center"
-        className={classes.splashTop}
-        style={{ zIndex: 10 }}
-      >
-        <Grid item xs={12} style={{ zIndex: 10 }}>
-          <Box align="center" pt={4}>
-            <img src="JunoLogo.svg" height="160px" alt="JUNO" />
-          </Box>
-          <Box align="center" pl={1} pr={1}>
-            <Typography variant="h5" component="h2">
-              A global genomic survey of <i>Streptococcus agalactiae</i>
-            </Typography>
-          </Box>
-        </Grid>
-      </Grid>
+    <Box width="100%" className={classes.splash}>
+      <Box align="center" pt={8}>
+        <img src="JunoLogo.svg" height="160px" alt="JUNO" />
+      </Box>
+      <Box align="center" pl={1} pr={1}>
+        <Typography variant="h5" component="h2">
+          A global genomic survey of <i>Streptococcus agalactiae</i>
+        </Typography>
+      </Box>
     </Box>
   );
 };
