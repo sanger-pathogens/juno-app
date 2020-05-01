8277ff59b336a713127fdc96ea21c85ed11a0af8
Gareth Peat <gp16@sanger.ac.uk>
2020-05-01 18:54:04 +0100

    Remove travis and use dockerhub's autobuild

--- docker_push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# build and tag
-docker build -t "sangerpathogens/juno-app$(jq -r '.version' package.json)" .
-
-# push
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push "sangerpathogens/juno-app$(jq -r '.version' package.json)"
